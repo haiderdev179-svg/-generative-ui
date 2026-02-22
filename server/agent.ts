9c96c00914a15f0d419341d5fb1eed379d61655b
haiderally <haiderdev179@gmail.com>
2026-02-22 14:20:58 +0500

    Setup database access in agent.ts + created tool node + give acess of tools to toolNode and LLM

--- server/agent.ts
+++ server/agent.ts
@@ -1,10 +1,18 @@
 import type { MessagesAnnotation } from "@langchain/langgraph";
 import { ChatOpenAI } from "@langchain/openai";
 import { initDB } from "./db.ts";
+import { initTools } from "./tool.ts";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
 
 
 //5:(Init database)
 const database = initDB("./expenses.db");
+
+//6:Giving tools access to database
+const tools = initTools(database);
+
+
+
 
 //1:Initialize the Model
 const llm = new ChatOpenAI({
@@ -13,11 +21,30 @@ const llm = new ChatOpenAI({
 });
 
 
+//7:Creating tool node
+const toolNode = new ToolNode(tools);
+
+
 
 //2:Creating (Call-Model)
 async function callModel(state: typeof MessagesAnnotation.State) {
 
+     //Giving tool access to LLM
+     const llmWithTools =  llm.bindTools(tools);
 
+     //Invoking the LLM and getting the response 
+     const response = await llmWithTools.invoke([
+        {
+            role: 'system', 
+             content: `You are a helpful expense tracking assistant. Current datetime: ${new Date().toISOString()}
+             Call add_expense tool to add the expense to database.
+             `
+        },
+        //sending the message history from state by spreading
+        ...state.messages
+     ]);
 
-    return state;
-}
+     //Here we are adding/concatinating this response to our messages history in the state
+    return {messages: [response]};
+};
+
