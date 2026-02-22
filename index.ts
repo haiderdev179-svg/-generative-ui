52701694b8281fc0573ef6a7eda6c80dd3e0d985
haiderally <haiderdev179@gmail.com>
2026-02-22 05:49:44 +0500

    Initialize the Model in (agent.ts)

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+import { ChatOpenAI } from "@langchain/openai";
+
+
+//Initialize the Model
+const llm = new ChatOpenAI({
+    model: 'gpt-5.1-mini',
+    temperature: 0,
+});
