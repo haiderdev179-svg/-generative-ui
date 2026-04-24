cca98a62057e17495a070f3499037c6b802e983a
haiderally <haiderdev179@gmail.com>
2026-04-25 00:52:17 +0500

    Setup SSE at client side

--- server.ts
+++ server.ts
@@ -11,6 +11,25 @@ app.get('/', (req, res)=> {
     res.json({message: 'OK'});
 });
 
+app.get('/chat', (req, res)=> {
+   
+    //SSE (Server Side Events)
+    //1: Add special header
+        res.writeHead(200, {
+          'Content-Type': 'text/event-stream'    
+        });
+        
+        setInterval(() => {
+            res.write("event: cgPing\n")
+            res.write("data: Happy Coding\n\n ")
+            
+        }, 1000);
+
+    //2: Send data in special format
+        
+    res.json({});
+});
+
 const PORT = process.env.PORT || 4100
 app.listen(PORT, ()=> console.log(`Server is running on http://localhost:${PORT}`));
 
